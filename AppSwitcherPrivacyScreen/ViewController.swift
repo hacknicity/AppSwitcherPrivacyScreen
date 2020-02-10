016863a7ce7bc7040622bdc26ad13624ee61b4d1
Geoff Hackworth <geoff@hacknicity.com>
2020-02-10 16:55:20 +0000

    Initial Commit. Xcode 11.3's Single View App template. Removed my Team name from the Signing & Capabilities section.

---
+++ AppSwitcherPrivacyScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppSwitcherPrivacyScreen
+//
+//  Created by Geoff Hackworth on 10/02/2020.
+//  Copyright © 2020 Hacknicity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
